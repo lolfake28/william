0b27f6beb1923d7c05311ce0dd5d354cfeff2557
William Morgan <williammorgan086@gmail.com>
2025-06-29 21:41:33 +0700

    fix site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'William Morgan',
     description:
       'William Morgan is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: '', // No trailing slash allowed!
+    siteUrl: 'https://www.linkedin.com/in/williammorgan28/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },

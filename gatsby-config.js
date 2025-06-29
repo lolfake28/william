34004e1aca20687b568bad36f8520bb3e3c53656
William Morgan <williammorgan086@gmail.com>
2025-06-29 21:53:43 +0700

    tes og1

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'William Morgan is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://www.linkedin.com/in/williammorgan28/', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/og1.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
   plugins: [
